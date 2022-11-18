5b8f9a834b009eeecda4b2422cbbcb57620e6959
Cez3xx <109895465+Cez3xx@users.noreply.github.com>
2022-11-18 13:00:50 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll('.btn')
 const screen = document.querySelector('.screen')
 const deleteButton = document.querySelector('.delete')
 const spaceButton = document.querySelector('.space')
-const uppercase = document.querySelector('.upper')
+const uppercase = document.querySelectorAll('.upper')
 
 buttons.forEach(function(button){
     button.addEventListener('click', function(e){
@@ -10,12 +10,16 @@ buttons.forEach(function(button){
         screen.value += value
     })
 })
-deleteButton.addEventListener('click', function(e){
+
+function setUppercase(){
+    buttons.forEach(button => button.classList.toggle('uppercase'))
+    
+}
+
+deleteButton.addEventListener('click', function(){
     screen.value = screen.value.substring(0, (screen.value).length - 1)
 })
 spaceButton.addEventListener('click', function(){
     screen.value += ' '
 })
-uppercase.addEventListener('click', function(){
-    buttons.classList.add('uppercase')
-})
+uppercase.forEach(uppercasebtn => addEventListener('click', setUppercase))
